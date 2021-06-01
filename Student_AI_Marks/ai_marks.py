52fe6f3882d10310fc441bc0307a3f941e263875
Riddhima Singhal <59498358+riddhima270406@users.noreply.github.com>
2021-06-01 15:35:18 +0530

    initial commit

---
+++ Student_AI_Marks/ai_marks.py
@@ -0,0 +1,47 @@
+student_list = ["Student 1", "Student 2", "Student 3", "Student 4", "Student 5", "Student 6", "Student 7", "Student 8", "Student 9", "Student 10"]
+  
+print(student_list[0])
+print(student_list[1])
+print(student_list[2])
+print(student_list[3])
+print(student_list[4])
+print(student_list[5])
+print(student_list[6])
+print(student_list[7])
+print(student_list[8])
+print(student_list[9])
+
+
+# s1_marks = input("Enter Student 1 marks: ")
+# s2_marks = input("Enter Student 2 marks: ")
+# s3_marks = input("Enter Student 3 marks: ")
+# s4_marks = input("Enter Student 4 marks: ")
+# s5_marks = input("Enter Student 5 marks: ")
+# s6_marks = input("Enter Student 6 marks: ")
+# s7_marks = input("Enter Student 7 marks: ")
+# s8_marks = input("Enter Student 8 marks: ")
+# s9_marks = input("Enter Student 9 marks: ")
+# s10_marks = input("Enter Student 10 marks: ")
+
+more_student = 1
+no_student = 0
+
+student = input("Press '1' if you wanna add more students otherwise press '0': ")
+# no_of_student = input("How many Students do you want to add: ")
+
+if(student == more_student):
+    # more_student = more_student + 1
+    print("bdh")
+
+elif(student == no_student):
+    print("no _smore_student")
+
+print("rod")
+
+# if (more_student == 1):
+#     s_name = input("Enter student name: ")
+#     s_marks = input("Enter "+s_name+" marks: ")
+
+# elif(more_student != 1):
+#     print("5678")
+
